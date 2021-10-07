ae75a571f8da53154182da4467ac5ff7a8440d3e
GabrielGrld <gbrlgrld@gmail.com>
2021-10-07 11:46:28 -0500

    add Your like in weeks calculator

---
+++ code/02_03_Your_life_in_weeks.py
@@ -0,0 +1,6 @@
+age = input ("How are old are you? ")
+years_left = 90 - int(age)
+days = years_left*365
+weeks = years_left*52
+months =years_left*12
+print(f"If you were to live until 90, you have {days} days, {weeks} weeks and {months} months left.")
