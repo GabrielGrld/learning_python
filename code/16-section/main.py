066cb0026f037e5e249a82660afc869668329b65
GabrielGrld <gbrlgrld@gmail.com>
2022-02-03 15:14:58 -0500

    rename folder adding a hyphen to eliminate spaces

--- code/16-section/main.py
+++ code/16-section/main.py
@@ -34,26 +34,22 @@ from money_machine import MoneyMachine
 
 is_on = True
 profit = 0
-while is_on:
-    menu = Menu()
-    choice = input(f"​What would you like? ({menu.get_items()}): ")
-    coffee_maker = CoffeeMaker()
-    money_machine = MoneyMachine()
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
 
+while is_on:
+    choice = input(f"​What would you like? ({menu.get_items()}): ")
     if choice == "report":
         coffee_maker.report()
         money_machine.report()
-
     elif choice == "off":
         is_on = False
     else:
         drink = menu.find_drink(choice)
-        if coffee_maker.is_resource_sufficient(drink):
-            money_machine.make_payment(drink.cost)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
             coffee_maker.make_coffee(drink)
-            print("se hace la bebida")
-        else:
-            print("no es suficiente")
+
 
 
 
