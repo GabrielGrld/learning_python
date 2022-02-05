9a5aafd4697903490a4a6d4599481064b12c9ff7
GabrielGrld <gbrlgrld@gmail.com>
2022-02-05 15:38:38 -0500

    create file for day 18- initial files

---
+++ code/18-section/main.py
@@ -0,0 +1,41 @@
+from turtle import Turtle, Screen
+
+jimmy = Turtle()
+jimmy.shape("turtle")
+jimmy.color("DeepSkyBlue", "LawnGreen")
+
+steps = 100
+angles = 90
+
+for _ in range(4):
+    jimmy.forward(steps)
+    jimmy.right(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
