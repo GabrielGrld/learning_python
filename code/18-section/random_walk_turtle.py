6862587b3850a0334c2396112be8b47f545666b5
GabrielGrld <gbrlgrld@gmail.com>
2022-02-06 10:39:43 -0500

    add random color to turtle using RGB colors

--- code/18-section/random_walk_turtle.py
+++ code/18-section/random_walk_turtle.py
@@ -8,6 +8,15 @@ colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSea
 orientation = [0, 90, 180, 270]
 
 jimmy = t.Turtle()
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r, g, b
+
+
 jimmy.shape("turtle")
 jimmy.color("DeepSkyBlue", "LawnGreen")
 
@@ -19,7 +28,7 @@ jimmy.pensize(10)
 
 def random_walk(_steps, _steps_length):
     for side in range(1, _steps+1):
-        jimmy.color(random.choice(colours))
+        jimmy.color(random_color())
         jimmy.seth(random.choice(orientation))
         jimmy.forward(_steps_length)
 
