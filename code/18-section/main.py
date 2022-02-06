6a3210a141da5b1a1ec78d58e625d1d1ca04edf0
GabrielGrld <gbrlgrld@gmail.com>
2022-02-06 10:22:13 -0500

    generate figures with increasing side number

--- code/18-section/main.py
+++ code/18-section/main.py
@@ -1,15 +1,43 @@
+# import turtle
+# tim = turtle.Turtle()
+
+# from turtle import Turtle
+#
+# tim = Turtle()
+# tom = Turtle()
+# terry = Turtle()
+
+
+import turtle as t
+# tim = t.Turtle()
+
+
+
 from turtle import Turtle, Screen
+import random
+
+colours = ["cyan", "purple", "white", "blue"]
 
 jimmy = Turtle()
 jimmy.shape("turtle")
 jimmy.color("DeepSkyBlue", "LawnGreen")
 
-steps = 100
-angles = 90
+sides = int(input("Sides to graph: "))
+side_length = int(input("Side Length: "))
 
-for _ in range(4):
-    jimmy.forward(steps)
-    jimmy.right(90)
+
+def draw_figure(side):
+    for side in range(1, side+1):
+        jimmy.forward(side_length)
+        jimmy.right(360/x)
+    jimmy.seth(0)
+
+
+for x in range(3, sides+1):
+    draw_figure(x)
+    jimmy.color(random.choice(colours))
+
+
 
 
 
