4fe63bc18277c01388ccd335c464350a7f568ca8
GabrielGrld <gbrlgrld@gmail.com>
2022-02-09 12:02:34 -0500

    use slice function to simplfiy line 47 in code

--- code/20-section/main.py
+++ code/20-section/main.py
@@ -44,10 +44,8 @@ while game_is_on:
         scoreboard.game_over()
         print("game over")
     # Detect collision with wall
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
 
