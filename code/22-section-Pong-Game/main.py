7a103ca9a92e2cc355001b67777b186a8c68c86e
GabrielGrld <gbrlgrld@gmail.com>
2022-02-11 12:12:08 -0500

    add scoreboard point

--- code/22-section-Pong-Game/main.py
+++ code/22-section-Pong-Game/main.py
@@ -1,7 +1,7 @@
-import turtle
-from turtle import Screen, Turtle
+from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,7 +12,7 @@ screen.title("Pong Game")
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
-
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -22,7 +22,7 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -38,8 +38,11 @@ while game_is_on:
     # detect R paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
+
     # detect L paddle misses:
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
