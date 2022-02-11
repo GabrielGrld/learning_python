854f818d9091b32a0495047d16a3bf9ecbe9b77e
GabrielGrld <gbrlgrld@gmail.com>
2022-02-11 14:24:51 -0500

    update snake game to store high score value

--- code/24-section-snake-with-score/scoreboard.py
+++ code/24-section-snake-with-score/scoreboard.py
@@ -2,12 +2,16 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
 
+with open("data.txt", mode="r" ) as file:
+    h_score = int(file.read())
+
 
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = h_score
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -15,13 +19,19 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score}  High Score {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.high_score))
+        self.score = 0
+        self.update_scoreboard()
+
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
+
