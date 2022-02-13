9eef183ad3ac72d654ff5faa5505f81d30bb947b
GabrielGrld <gbrlgrld@gmail.com>
2022-02-12 22:08:21 -0500

    create file to read csv data and use of Pandas library

---
+++ code/25-section-CSV-Pandas/main.py
@@ -0,0 +1,41 @@
+# with open("weather_data.csv") as data_file:
+#     data = data_file.readlines()
+#     print(data)
+
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
+
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+# print(type(data))
+# print(type(data["temp"]))
+
+# data_dict = data.to_dict()
+# print(data_dict)
+# print(data_dict["temp"])
+# temp_list = data["temp"].to_list()
+# mean_series = data["temp"].mean()
+# print(mean_series)
+# print(sum(temp_list)/len(temp_list))
+# print(f"Max Temp value is: {data['temp'].max()}")
+
+
+# Get Data in Row
+# This can be useful if we need ot filter something, in this case, we filter the days that are equal to Monday
+# in the second print we are filtering the max value in the dataset, using the function max()
+
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+monday = data[data.day == "Monday"]
+monday_temp = int(monday.temp)
+print(monday_temp*(9/5)+32)
