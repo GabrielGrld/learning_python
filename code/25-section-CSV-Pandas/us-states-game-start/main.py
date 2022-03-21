60e1f490ffe98e3f13e15092534446e828bee3d6
GabrielGrld <gbrlgrld@gmail.com>
2022-03-20 22:25:28 -0500

    previous work

--- code/25-section-CSV-Pandas/us-states-game-start/main.py
+++ code/25-section-CSV-Pandas/us-states-game-start/main.py
@@ -1,8 +1,34 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
 
+data = pandas.read_csv("50_states.csv")
+# all_states = data["state"].to_list()   #this line is the same that the previous one
+all_states = data.state.to_list()
+guessed_states = []
 
-screen.addshape()
+while len(guessed_states)<50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name? ").title()
+    print(answer_state)
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        print(missing_states)
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]  # this will pull out the row where answer_state is equal to state
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())  # graph the first item in the row, only it, from the data frame
+
