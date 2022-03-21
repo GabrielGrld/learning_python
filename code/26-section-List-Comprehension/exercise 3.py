ddde54ab58a63ec00ccf46ec6cfa83964a0ae8bc
GabrielGrld <gbrlgrld@gmail.com>
2022-03-20 23:15:38 -0500

    exercise 3

--- code/26-section-List-Comprehension/exercise 3.py
+++ code/26-section-List-Comprehension/exercise 3.py
@@ -1,13 +1,21 @@
 
-file1 = open("file1.txt", "r")
-file2 = open("file2.txt", "r")
+# file1 = open("file1.txt", "r")
+# file2 = open("file2.txt", "r")
+# numbers_one = [int(number) for number in file1]
+# numbers_two = [int(number) for number in file2]
+# file1.close()
+# file2.close()
 
-numbers_one = [int(number) for number in file1]
-numbers_two = [int(number) for number in file2]
+with open("file1.txt") as file1:
+    numbers_one =file1.readlines()
+with open("file2.txt") as file2:
+    numbers_two = file2.readlines()
+
 
 print(numbers_one)
 print(numbers_two)
 # Write your code above 👆
 
-result = [n1 for n1 in numbers_one if n1 in numbers_two]
+result = [int(n1) for n1 in numbers_one if n1 in numbers_two]
 print(result)
+
